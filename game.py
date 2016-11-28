9c0f0da77560990b21bde7e15ba085bef1ad29d1
Gregory Leeman <gregoryleeman@outlook.com>
2016-11-28 11:39:19 +0000

    Add files via upload

---
+++ game.py
@@ -0,0 +1,17 @@
+# Author: Gregory Leeman
+# Title: Unnamed Game
+# Created on: 28 NOV 2016
+
+# Last edited on: 28 NOV 2016
+# Recent changes:
+
+"""
+
+"""
+
+import pygame
+import random
+
+class game:
+
+    def __init__(self):
